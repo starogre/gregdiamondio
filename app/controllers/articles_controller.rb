6966d0f092eedfbecfdf7dc2015ff942d0017be2
Greg Diamond <gregdiamond29@gmail.com>
2015-07-29 16:37:55 -0400

    Blog articles display on the root page now, and if you're logged in, you'll have the ability to post a new one from the same page. They're displayed in order of newness. This is not pretty at the moment, but hopefully it will be soon

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,20 @@
 class ArticlesController < ApplicationController
   def index
-    @articles = Article.all
+    @article = Article.new
+    @articles = Article.all.sort do |x,y|
+      y.created_at <=> x.created_at
+    end
   end
+
+   def create
+     @article = Article.new(article_params)
+     @article.save
+
+     redirect_to '/'
+   end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
 end
