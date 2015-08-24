632cbb16e9dad629979a343e937745fb1a643168
Greg Diamond <gregdiamond29@gmail.com>
2015-08-24 14:30:29 -0400

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -27,7 +27,6 @@ ActiveRecord::Schema.define(version: 20150824182237) do
   create_table "projects", force: :cascade do |t|
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.string   "projects"
     t.string   "title"
     t.text     "description"
   end
